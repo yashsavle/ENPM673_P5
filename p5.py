bc89a1232b3ec2ef0a77c68d3fd896dce44958af
yashsavle <yashsavle@gmail.com>
2020-05-01 01:11:17 -0400

    Completed Code-Working,videoleft

--- p5.py
+++ p5.py
@@ -3,21 +3,29 @@ import numpy as np
 from ReadCameraModel import ReadCameraModel
 from UndistortImage import UndistortImage
 import math
-
+import glob
+import os
+import matplotlib.pyplot as plt
+import random
+import time
+import pandas as pd
 
 # Store the Dataset as a list
 path = 'stereo/centre/'
-f = [img for img in glob.glob(path)]
-f.sort()
-
+#f = [img for img in glob.glob(path)]
+f=[]
+for img in os.listdir(path):
+	f.append(img)
+	f.sort()
+print(len(f))
 
 # Extract Camera Parameters
-fx, fy , cx , cy , G camera image, LUT = ReadCameraModel('./model')
+fx, fy , cx , cy ,camera_image, LUT = ReadCameraModel('./model')
 
 # Calibration Matrix
 K =  np.array([[fx , 0 , cx], [0 , fy ,cy], [0 , 0 , 1]])
 
-
+file1 = open("points.txt","a")
 
 # SIFT to find Points of Correspondence
 
@@ -28,119 +36,120 @@ def findpts(img1,img2):
 
 	# Determine Key Points and Descriptors
 	kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+	kp2, des2 = sift.detectAndCompute(img2,None)
 
-    # FLANN
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks=50)
+	# FLANN
+	FLANN_INDEX_KDTREE = 0
+	index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+	search_params = dict(checks=50)
 
-    flann = cv2.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
+	flann = cv2.FlannBasedMatcher(index_params,search_params)
+	matches = flann.knnMatch(des1,des2,k=2)
 
-    # Filter Accurate Matches
-    match_filter = [[(0,0) for i in range(len(matches))]]
+	# List to store features for two frames
+	f1 = []
+	f2 = []
 
-    # Ratio Test
-    X = []
-    ratio = 0.5
-    pts = []
-    for i,(m,n) in enumerate(matches):
-        if m.distance < ratio*n.distance:
-            match_filter[i]=[1,0]
-            X.append(m)
-            pts.append([kp1[m.queryIdx].pt, kp2[m.trainIdx].pt]) 
-    pts = np.array(pts)
+	# Ratio Test
+	X = []
+	ratio = 0.5
+	pts = []
+	for i,(m,n) in enumerate(matches):
+		if m.distance < ratio*n.distance:
+			f1.append(kp1[m.queryIdx].pt)
+			f2.append(kp2[m.trainIdx].pt)
 
-    return pts
+	return f1,f2
 
 
-def fundamental_mat(gf1,gf2):
-    A = np.empty((8, 9))
 
-    for i in range(0, len(corners1)): # Looping over all the 8-points (features)
-        x1 = corners1[i][0] # x-coordinate from current frame 
-        y1 = corners1[i][1] # y-coordinate from current frame
-        x2 = corners2[i][0] # x-coordinate from next frame
-        y2 = corners2[i][1] # y-coordinate from next frame
-        A[i] = np.array([x1*x2, x2*y1, x2, y2*x1, y2*y1, y2, x1, y1, 1])
 
-    u, s, v = np.linalg.svd(A, full_matrices=True)  # Taking SVD of the matrix
-    f = v[-1].reshape(3,3) # Last column of V matrix
-    
-    u1,s1,v1 = np.linalg.svd(f) 
-    s2 = np.array([[s1[0], 0, 0], [0, s1[1], 0], [0, 0, 0]]) # Constraining Fundamental Matrix to Rank 2
-    F = u1 @ s2 @ v1  
-    
-    return F  
+def findF(f1,f2):
+	total = 0
+	F_new = np.zeros((3,3))
+	i1 = []
+	i2 = []
+	it = 10 # No. of iterations for RANSAC
+	j=0
+	for j in range(0,it):
+		#print(j)
+		c = 0
+		pts = []
+		frame1 = []
+		frame2 = []
+		tmp1 = []
+		tmp2 = []
+
+        # 8 point Algorithm
+		while True:
+			n = random.randint(0,len(f1)-1)
+			if n not in pts:
+				pts.append(n)
+			if len(pts) == 8:
+				break
+
+		#print (pts)
+        # update features
+		for p in pts:
+			frame1.append([f1[p][0], f1[p][1]])
+			frame2.append([f2[p][0], f2[p][1]])
+
+		F = run_ransac(frame1,frame2)
+		#print(F)
+		for k in range(0, len(f1)):
+			#print(k)
+			if checkval(f1[k], f2[k],F) < 0.01:
+				c+=1
+				#print('a\n')
+				tmp1.append(f1[k])
+				tmp2.append(f2[k])
+		if c > total:
+			total = c
+			F_new = F
+			i1 = tmp1
+			i2 = tmp2
+		
+	return F,i1,i2
+
+
+def run_ransac(f1,f2):
+	A = np.empty((8, 9))
+
+	# Store 8 feature points in A matrix
+	for i in range(0, len(f1)):
+		x1 = f1[i][0]  
+		y1 = f1[i][1] 
+		x2 = f2[i][0] 
+		y2 = f2[i][1] 
+		A[i] = np.array([x1*x2, x2*y1, x2, y2*x1, y2*y1, y2, x1, y1, 1])
+
+	# Take SVD to solve for F
+	u, s, v = np.linalg.svd(A, full_matrices=True)  
+	# Extract Solution 
+	f = v[-1].reshape(3,3)
+
+	# Remove noise by changing Rank to 2
+	u1,s1,v1 = np.linalg.svd(f) 
+	s2 = np.array([[s1[0], 0, 0], [0, s1[1], 0], [0, 0, 0]])
+	F = u1 @ s2 @ v1  
+
+	return F  
 
 
 
 def checkval(x1, x2 , F):
-    x11=np.array([x1[0],x1[1],1]).T
-    x22=np.array([x2[0],x2[1],1])
-    return abs(np.squeeze(np.matmul((np.matmul(x22,F)),x11)))
-
-
-def use_ransac(pts):
-
-	numpts = pts.shape[0]
-	p_axis = np.ones((numpts,1))
-
-	pts1 = np.concatenate((pts[:,0,:],p_axis),axis = 1)
-	pts2 = np.concatenate((pts[:,1,:],p_axis),axis = 1)
-
-	n_inliers = 0
-	final_F = np.zeros((3,3))
-		
-	iter = 50
-	i1 = []
-	i2 = []
-
-	while iter >0:
-		count = 0
-		eightpoint = []
-		good_features1 = []
-		good_features2 = []
-		tmp1 = []
-		tmp2 = []
-
-		while(True):
-            num = random.randint(0, len(pts1)-1)
-            if num not in eightpoint:
-                eightpoint.append(num)
-            if len(eightpoint) == 8:
-                break
-
-
-        for point in eightpoint: # Looping over eight random points
-            good_features1.append([pts1[point][0], pts1[point][1]]) 
-            good_features2.append([pts2[point][0], pts2[point][1]])
-
-
-        # Fundamental Matrix
-        F = fundamental_mat(good_features1,good_features2)
-
-        for n in range(0,len(pts1)):
-
-        	if checkval(good_features1[n], good_features2[n],F)<0.01:
-        		count = count + 1
-        		tmp1.append(pts1[n])
-        		tmp2.append(pts2[n])
-
-
-        	if count > n_inliers:
-        		n_inliers = count
-        		final_F = F
-        		i1 = tmp1 
-        		i2 = tmp2
-    return final_F,i1,i2
+	a=np.array([x1[0],x1[1],1]).T
+	b=np.array([x2[0],x2[1],1])
+	return abs(np.squeeze(np.matmul((np.matmul(b,F)),a)))
 
 
 
 
-def findE(K,F):
-	t = np.matmul(np.matmul(K.T,F),K)
+
+
+# calculate the Essential Matrix with F and Camera Params
+def findE(cal,F):
+	t = np.matmul(np.matmul(cal.T,F),K)
 	u,s,v = np.linalg.svd(t,full_matrices=True)
 	sigmaF = np.array([[1,0,0],[0,1,0],[0,0,0]])
 	a = np.matmul(u,sigmaF)
@@ -150,104 +159,179 @@ def findE(K,F):
 
 
 
-def findposeE(essentialMatrix):
-	u, s, v = np.linalg.svd(essentialMatrix, full_matrices=True)
-    w = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
-    
-    # 1st Solution
-    c1 = u[:, 2] 
-    r1 = u @ w @ v
-    
-    if np.linalg.det(r1) < 0:
-        c1 = -c1 
-        r1 = -r1
-    c1 = c1.reshape((3,1))
-    
-    # 2nd Solution
-    c2 = -u[:, 2]
-    r2 = u @ w @ v
-    if np.linalg.det(r2) < 0:
-        c2 = -c2 
-        r2 = -r2 
-    c2 = c2.reshape((3,1))
-    
-    # 3rd Solution
-    c3 = u[:, 2]
-    r3 = u @ w.T @ v
-    if np.linalg.det(r3) < 0:
-        c3 = -c3 
-        r3 = -r3 
-    c3 = c3.reshape((3,1)) 
-    
-    # 4th Solution
-    c4 = -u[:, 2]
-    r4 = u @ w.T @ v
-    if np.linalg.det(r4) < 0:
-        c4 = -c4 
-        r4 = -r4 
-    c4 = c4.reshape((3,1))
-    
-    return [r1, r2, r3, r4], [c1, c2, c3, c4]
+def decomp(E):
+	u, s, v = np.linalg.svd(E, full_matrices=True)
+	w = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+
+	# Configuration 1
+	c1 = u[:, 2] 
+	r1 = u @ w @ v
+
+	# Correct Camera Pose if determinant < 0
+	if np.linalg.det(r1) < 0:
+		c1 = -c1 
+		r1 = -r1
+	c1 = c1.reshape((3,1))
+
+	# Configuration 2
+	c2 = -u[:, 2]
+	r2 = u @ w @ v
+	if np.linalg.det(r2) < 0:
+		c2 = -c2 
+		r2 = -r2 
+	c2 = c2.reshape((3,1))
+
+	# Configuration 3
+	c3 = u[:, 2]
+	r3 = u @ w.T @ v
+	if np.linalg.det(r3) < 0:
+		c3 = -c3 
+		r3 = -r3 
+	c3 = c3.reshape((3,1)) 
+
+	# Configuration 4
+	c4 = -u[:, 2]
+	r4 = u @ w.T @ v
+	if np.linalg.det(r4) < 0:
+		c4 = -c4 
+		r4 = -r4 
+	c4 = c4.reshape((3,1))
+
+	return [r1, r2, r3, r4], [c1, c2, c3, c4]
 
 
-def bestRT(Rlist, Clist, features1, features2):
-    check = 0
-    Horigin = np.identity(4) # current camera pose is always considered as an identity matrix
-    for index in range(0, len(Rlist)): # Looping over all the rotation matrices
-        angles = rotationMatrixToEulerAngles(Rlist[index]) # Determining the angles of the rotation matrix
-        #print('angle', angles)
-        
-        # If the rotation of x and z axis are within the -50 to 50 degrees then it is considered down in the pipeline 
-        if angles[0] < 50 and angles[0] > -50 and angles[2] < 50 and angles[2] > -50: 
-            count = 0 
-            newP = np.hstack((Rlist[index], Clist[index])) # New camera Pose 
-            for i in range(0, len(features1)): # Looping over all the inliers
-                temp1x = getTriangulationPoint(Horigin[0:3,:], newP, features1[i], features2[i]) # Triangulating all the inliers
-                thirdrow = Rlist[index][2,:].reshape((1,3)) 
-                if np.squeeze(thirdrow @ (temp1x - Clist[index])) > 0: # If the depth of the triangulated point is positive
-                    count = count + 1 
+def cheirality_check(R, C, i1, i2):
+	check = 0
 
-            if count > check: 
-                check = count
-                mainc = Clist[index]
-                mainr = Rlist[index]
-                
-    if mainc[2] > 0:
-        mainc = -mainc
-                
-    #print('mainangle', rotationMatrixToEulerAngles(mainr))
-    return mainr, mainc
+	H = np.identity(4) 
+	for i in range(0, len(R)): 
+
+	    # find angles from R
+		angles = get_orientation(R[i])
+
+
+		if angles[0] < 50 and angles[0] > -50 and angles[2] < 50 and angles[2] > -50:
+			count = 0 
+			newP = np.hstack((R[i], C[i])) 
+			for j in range(0, len(i1)): 
+				X = linear_triangulation(H[0:3,:], newP, i1[j], i2[j]) 
+				tr = R[i][2,:].reshape((1,3)) 
+				if np.squeeze(tr @ (X - C[i])) > 0: 
+					count = count + 1 
+
+			if count > check: 
+					check = count
+					newC = C[i]
+					newR = R[i]
+
+	if newC[2] > 0:
+		newC = -newC
+
+
+	return newR, newC
+
+# Find angles
+def get_orientation(R):
+	angle = math.sqrt(R[0,0]**2+ R[1,0]**2)
+	flag = angle< 1e-6
+
+	if not flag:
+		x = math.atan2(R[2,1],R[2,2])
+		y = math.atan2(-R[2,0],angle)
+		z = math.atan2(R[1,0],R[0,0])
+
+	else :
+		x = math.atan2(-R[1,2], R[1,1])
+		y = math.atan2(-R[2,0], angle)
+		z = 0
+	return np.array([x*180/math.pi, y*180/math.pi, z*180/math.pi])
+
+# Perform Linear Triangulation
+def linear_triangulation(pose1,pose2,pt1,pt2):
+	x_i = np.array([[0, -1, pt1[1]], [1, 0, -pt1[0]], [-pt1[1], pt1[0], 0]])
+	x_i1= np.array([[0, -1, pt2[1]], [1, 0, -pt2[0]], [-pt2[1], pt2[0], 0]])
+	A1 = x_i @ pose1[0:3, :] 
+	A2 = x_i1@ pose2
+	Ax = np.vstack((A1, A2))
+	u, s, v = np.linalg.svd(Ax)
+	new_X = v[-1]
+	new_X = new_X/new_X[3]
+	new_X = new_X.reshape((4,1))
+	return new_X[0:3].reshape((3,1))   
+
+
+# Homogeneous Matrix
+def get_homogeneous(R, T):
+	a = np.column_stack((R, T))
+	b = np.array([0, 0, 0, 1])
+	H = np.vstack((a, b))
+	return H
 
 # Main
 
 # Preprocess Images
-
-for i in range(0,len(f)-1):
-
-	img1 = cv2.imread(f[i])
+start_t = time.time()
+H_init = np.identity(4)
+p0 = np.array([[0,0,0,1]]).T
+data = [] 
+for i in range(19,25):
+	print(i)
+	img1 = cv2.imread(path+str(f[i]),0)
+	# cv2.imshow("",img1)
+	# cv2.waitKey(0)
 	bgr_img1 = cv2.cvtColor(img1, cv2.COLOR_BayerGR2BGR)
-	undist_img1 = UndistortImage(img1,LUT)
+	undist_img1 = UndistortImage(bgr_img1,LUT)
 	gray_img1 = cv2.cvtColor(undist_img1,cv2.COLOR_BGR2GRAY)
 
 
-	img2 = cv2.imread(f[i+1])
-	bgr_img2 = cv2.cvtColor(img1, cv2.COLOR_BayerGR2BGR)
-	undist_img2 = UndistortImage(img1,LUT)
-	gray_img2 = cv2.cvtColor(undist_img1,cv2.COLOR_BGR2GRAY)
+	img2 = cv2.imread(path+str(f[i+1]),0)
+	bgr_img2 = cv2.cvtColor(img2, cv2.COLOR_BayerGR2BGR)
+	undist_img2 = UndistortImage(bgr_img2,LUT)
+	gray_img2 = cv2.cvtColor(undist_img2,cv2.COLOR_BGR2GRAY)
 
+	gray_img1 = gray_img1[200:650 , 0:1280]
+	gray_img2 = gray_img2[200:650 , 0:1280]
 
 	# Find Points of Correspondence 
 
-	pts = findpts(gray_img1,gray_img2)
+	f1,f2 = findpts(gray_img1,gray_img2)
+	for l in range(0,len(f1)):
+		a = (int(f1[l][0]) , int(f1[l][1]))
+		sift_img = cv2.circle(gray_img1, a,1,(255,0,0),3)
+	# sift_img = plt.imread(sift_img)
+	# plt.imshow("",sift_img)
+	# cv2.waitKey(0)
+	# #print(f2)
+	# Determine F
 
-
-	# Estimate  Fundamental Matrix using RANSAC
-
-	F, i1 ,i2 = use_ransac(pts)
+	F, i1 ,i2 = findF(f1,f2)
 	E = findE(K, F)
 
+	#print(E)
 	# Find T and R
-	R_list, T_list = findposeE(E)
-	R , T = bestRT(R_list,T_list,i1,i2)
+	R_list, T_list = decomp(E)
+	R , T = cheirality_check(R_list,T_list,i1,i2)
+
+    # Find Homogeneous Matrix and Camera Centre for Each frame
+
+	H = get_homogeneous(R,T)
+	H_init = H_init @ H
+	p = H_init @ p0
+
+	print('X = ',p[0])
+	print('Y = ',p[2])
+	data.append([p[0][0], -p[2][0]])
+	file1.write(str(p[0][0])+",")
+	file1.write(str(-p[2][0])+"\n")
+	plt.scatter(p[0][0], -p[2][0], color='b')
+file1.close()
+end_t = time.time()
+delta = (end_t - start_t)/60
+print("Time taken: ",delta)
+df = pd.DataFrame(data, columns = ['X', 'Y'])
+df.to_excel('coordinates.xlsx')
+plt.show()
+
+
 
 
